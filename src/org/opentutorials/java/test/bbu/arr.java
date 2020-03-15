fa0109c077d70e1603f08872b017b036b30eaf42
yuna706 <riyu6027@naver.com>
2020-03-15 22:58:54 +0900

    잉

--- src/org/opentutorials/java/test/bbu/arr.java
+++ src/org/opentutorials/java/test/bbu/arr.java
@@ -6,12 +6,11 @@ public class arr {
 
 	public static void main(String[] args) {
 		int[] arr = new int[5];
-		int test = 5;
 		
-		Scanner aa = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		for (int i = 0; i < arr.length; i++) {
-			int a = aa.nextInt();
+			int a = sc.nextInt();
 			arr[i] = a*10;
 			System.out.println(arr[i]);
 		}
